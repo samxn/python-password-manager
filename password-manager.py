610ddea4bf7f6fd8287a0cc8feda5493f8672937
Samin <86338112+samxn@users.noreply.github.com>
2023-02-12 23:07:16 -0500

    added add and view options

--- password-manager.py
+++ password-manager.py
@@ -1,20 +1,26 @@
-pwd = input("What is the master password? ")
+master_pwd = input("What is the master password? ")
 
 def view():
-    pass
+     with open('passwords.txt', 'r') as f:
+         for line in f.readlines():
+             print(line.rstrip())
 
 def add():
-    pass
+    name = input('username: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + " | " + pwd + "\n")
 
 while True:
-    mode = input("Would you like to add a new password or view an existing password? (view/add), press q to quit").lower()
+    mode = input("Would you like to add a new password or view an existing password? (view/add), press q to quit. ").lower()
     if mode == "q":
         break
     
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid mode.")
         continue
