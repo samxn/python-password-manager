57cd2866112c2bbafed0477c28eac2676a943f1e
Samin <86338112+samxn@users.noreply.github.com>
2023-02-12 23:40:33 -0500

    Added encode/decode

--- password-manager.py
+++ password-manager.py
@@ -1,7 +1,5 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
-
 '''
 def write_key():
     key = Fernet.generate_key()
@@ -9,19 +7,32 @@ def write_key():
         key_file.write(key)
 '''
 
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+master_pwd = input("What is the master password? ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
 def view():
      with open('passwords.txt', 'r') as f:
          for line in f.readlines():
              data = line.rstrip()
              user, passw = data.split("|")
-             print("User:", user, "| Password:", passw)
+             print("User:", user, "| Password:",  fer.decrypt(passw.encode())).decode()
+
+
 
 def add():
     name = input('Username: ')
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view an existing password? (view/add), press q to quit. ").lower()
