1d2ccae6c16342e93fa933ccea413165a8bb15fb
Samin <86338112+samxn@users.noreply.github.com>
2023-02-12 23:27:07 -0500

    Created key file code

--- password-manager.py
+++ password-manager.py
@@ -1,16 +1,25 @@
+from cryptography.fernet import Fernet
+
 master_pwd = input("What is the master password? ")
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
 
 def view():
      with open('passwords.txt', 'r') as f:
          for line in f.readlines():
-             print(line.rstrip())
+             data = line.rstrip()
+             user, passw = data.split("|")
+             print("User:", user, "| Password:", passw)
 
 def add():
-    name = input('username: ')
+    name = input('Username: ')
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + " | " + pwd + "\n")
+        f.write(name + "|" + pwd + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view an existing password? (view/add), press q to quit. ").lower()
