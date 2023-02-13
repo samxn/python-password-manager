5902053a4f1c84802475a728da40a1288cdd5bbd
Samin <86338112+samxn@users.noreply.github.com>
2023-02-12 23:30:48 -0500

    Created master key

--- password-manager.py
+++ password-manager.py
@@ -2,10 +2,12 @@ from cryptography.fernet import Fernet
 
 master_pwd = input("What is the master password? ")
 
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
+'''
 
 def view():
      with open('passwords.txt', 'r') as f:
